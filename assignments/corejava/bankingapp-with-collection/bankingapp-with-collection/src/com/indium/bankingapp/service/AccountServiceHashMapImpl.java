95a664b1723825dc62fc894f99ccaebb79a84795
surajn198 <nandasuraj198@gmail.com>
2023-09-11 11:38:57 +0530

    HashMap Impl

---
+++ assignments/corejava/bankingapp-with-collection/bankingapp-with-collection/src/com/indium/bankingapp/service/AccountServiceHashMapImpl.java
@@ -0,0 +1,47 @@
+package com.indium.bankingapp.service;
+
+import com.indium.bankingapp.model.Account;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class AccountServiceHashMapImpl implements AccountService {
+    private HashMap<Integer, Account> accountsMap = new HashMap<>();
+
+    @Override
+    public boolean createAccount(Account account) {
+        if (!accountsMap.containsKey(account.getId())) {
+            accountsMap.put(account.getId(), account);
+            return true; // Account created successfully
+        }
+        return false; // Account with the same ID already exists
+    }
+
+    @Override
+    public boolean updateAccount(int id, Account updatedAccount) {
+        if (accountsMap.containsKey(id)) {
+            accountsMap.put(id, updatedAccount);
+            return true; // Account updated successfully
+        }
+        return false; // Account not found
+    }
+
+    @Override
+    public boolean deleteAccount(int id) {
+        if (accountsMap.containsKey(id)) {
+            accountsMap.remove(id);
+            return true; // Account deleted successfully
+        }
+        return false; // Account not found
+    }
+
+    @Override
+    public Account getAccount(int id) {
+        return accountsMap.get(id);
+    }
+
+    @Override
+    public Collection<Account> getAllAccounts() {
+        return accountsMap.values();
+    }
+}
