48654faa5f03b80e0ca4cd63adf906d7adada335
surajn198 <nandasuraj198@gmail.com>
2023-09-18 20:31:41 +0530

    Assignment 9- impexp-thread-AccountService

---
+++ assignments/corejava/bankingapp-with-impexp-thread/bankingapp-with-impexp-thread/src/com/indium/bankingapp/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.indium.bankingapp.service;
+
+import com.indium.bankingapp.model.Account;
+
+import java.util.Collection;
+
+public interface AccountService<T> {
+    boolean createAccount(T account);
+    boolean updateAccount(int id, T account);
+    boolean deleteAccount(int id);
+    T getAccount(int id);
+    Collection<T> getAllAccounts();
+
+    // method for importing accounts
+    boolean importAccounts(String filePath);
+  
+    
+    // method for exporting accounts
+    boolean exportAccounts(String filePath);
+    
+}
