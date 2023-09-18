28f73ff29a427d47d9ba73b803b9679785026e9b
surajn198 <nandasuraj198@gmail.com>
2023-09-18 16:00:06 +0530

    Assignment 8 - impexp - BankingAppMain

---
+++ assignments/corejava/bankingapp-with-impexp/bankingapp-with-impexp/src/com/indium/bankingapp/BankingAppMain.java
@@ -0,0 +1,454 @@
+package com.indium.bankingapp;
+
+import com.indium.bankingapp.model.Account;
+import com.indium.bankingapp.service.AccountService;
+import com.indium.bankingapp.service.AccountServiceHashMapImpl;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankingAppMain {
+    private static AccountService<Account> accountService = createAccountService();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            displayMenu();
+            int choice = getUserChoice();
+
+            try {
+                switch (choice) {
+                    case 1:
+                        captureAccountDetails();
+                        break;
+                    case 2:
+                        viewAllAccounts();
+                        break;
+                    case 3:
+                        viewAccountDetails();
+                        break;
+                    case 4:
+                        updateAccountDetails();
+                        break;
+                    case 5:
+                        deleteAccount();
+                        break;
+                    case 6:
+                        handlePrintStatisticsMenu();
+                        break;
+                    case 7:
+                        importAccounts();
+                        break;
+                    case 8:
+                        exportAccounts();
+                        break;
+                    case 9:
+                        System.out.println("Exiting the Banking App.");
+                        scanner.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid choice.");
+                scanner.nextLine(); // Consume the invalid input
+            } catch (Exception e) {
+                System.err.println("An error occurred: " + e.getMessage());
+            }
+        }
+    }
+
+    private static AccountService<Account> createAccountService() {
+        return new AccountServiceHashMapImpl();
+    }
+
+    private static void displayMenu() {
+        System.out.println("\nBanking App Menu:");
+        System.out.println("1] Add Account");
+        System.out.println("2] View All Accounts");
+        System.out.println("3] View Account");
+        System.out.println("4] Update Account");
+        System.out.println("5] Delete Account");
+        System.out.println("6] Print Statistics");
+        System.out.println("7] Import");
+        System.out.println("8] Export");
+        System.out.println("9] Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    private static int getUserChoice() {
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        return choice;
+    }
+
+    private static void captureAccountDetails() {
+        try {
+            System.out.print("Enter Account Type (savings/deposit/loan): ");
+            String accountType = scanner.nextLine().toLowerCase();
+
+            if (!isValidAccountType(accountType)) {
+                System.out.println("Invalid account type. Please enter 'savings', 'deposit', or 'loan'.");
+                return;
+            }
+
+            System.out.print("Enter Account ID: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            // Validate that ID is positive
+            if (id <= 0) {
+                System.out.println("Invalid account ID. Please enter a positive integer.");
+                return;
+            }
+
+            System.out.print("Enter Account Name: ");
+            String name = scanner.nextLine();
+
+            // Validate that the name is not empty
+            if (name.isEmpty()) {
+                System.out.println("Account name cannot be empty.");
+                return;
+            }
+
+            System.out.print("Enter Initial Balance: ");
+            double balance = scanner.nextDouble();
+            scanner.nextLine();
+
+            // Validate that the balance is non-negative
+            if (balance < 0) {
+                System.out.println("Initial balance cannot be negative.");
+                return;
+            }
+
+            System.out.print("Enter Rate of Interest (ROI): ");
+            double roi = scanner.nextDouble();
+            scanner.nextLine();
+
+            // Validate that the ROI is non-negative
+            if (roi < 0) {
+                System.out.println("Rate of Interest (ROI) cannot be negative.");
+                return;
+            }
+
+            Account account = new Account(id, name, balance, roi, accountType);
+
+            // Calculate and update the balance with interest
+            calculateInterestAndUpdateBalance(account);
+
+            if (accountService.createAccount(account)) {
+                System.out.println("Account created successfully.");
+            } else {
+                System.out.println("Failed to create an account.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid data.");
+            scanner.nextLine(); // Consume the invalid input
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+    private static void calculateInterestAndUpdateBalance(Account account) {
+        double balance = account.getBalance();
+        double roi = account.getRoi();
+        double interest = balance * roi;
+
+        // Add interest to the balance
+        double newBalance = balance + interest;
+        account.setBalance(newBalance); // Update the balance attribute directly
+    }
+
+    private static boolean isValidAccountType(String accountType) {
+        return accountType.equals("savings") || accountType.equals("deposit") || accountType.equals("loan");
+    }
+
+    private static void viewAllAccounts() {
+        List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+
+        if (!accounts.isEmpty()) {
+            System.out.println("All Accounts:");
+            for (Account account : accounts) {
+                System.out.println(account.toString());
+                System.out.println("--------------");
+            }
+        } else {
+            System.out.println("No accounts found.");
+        }
+    }
+
+    private static void viewAccountDetails() {
+        try {
+            System.out.print("Enter Account ID to view: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            Account account = accountService.getAccount(id);
+
+            if (account != null) {
+                System.out.println("Account Details:");
+                System.out.println(account.toString());
+            } else {
+                System.out.println("Account not found.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid ID.");
+            scanner.nextLine(); // Consume the invalid input
+        }
+    }
+
+    private static void updateAccountDetails() {
+        try {
+            System.out.print("Enter Account ID to update: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            Account existingAccount = accountService.getAccount(id);
+
+            if (existingAccount != null) {
+                System.out.print("Enter New Account Name: ");
+                String newName = scanner.nextLine();
+                System.out.print("Enter New Account Balance: ");
+                double newBalance = scanner.nextDouble();
+                scanner.nextLine();
+
+                Account updatedAccount = new Account(id, newName, newBalance, existingAccount.getRoi(), existingAccount.getAccountType());
+
+                if (accountService.updateAccount(id, updatedAccount)) {
+                    System.out.println("Account updated successfully.");
+                } else {
+                    System.out.println("Failed to update the account.");
+                }
+            } else {
+                System.out.println("Account not found.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid data.");
+            scanner.nextLine(); // Consume the invalid input
+        }
+    }
+
+    private static void deleteAccount() {
+        try {
+            System.out.print("Enter Account ID to delete: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            if (accountService.deleteAccount(id)) {
+                System.out.println("Account deleted successfully.");
+            } else {
+                System.out.println("Failed to delete the account.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid ID.");
+            scanner.nextLine(); // Consume the invalid input
+        }
+    }
+
+    private static void handlePrintStatisticsMenu() {
+        while (true) {
+            displayStatisticsMenu();
+            int choice = getUserChoice();
+
+            try {
+                switch (choice) {
+                    case 1:
+                        printAccountsAbove100000();
+                        break;
+                    case 2:
+                        printAccountTypeCounts();
+                        break;
+                    case 3:
+                        printSortedAccountTypeCounts();
+                        break;
+                    case 4:
+                        printAvgBalanceByAccountType();
+                        break;
+                    case 5:
+                        listAccountIdsByName();
+                        break;
+                    case 6:
+                        return; // Return to the main menu
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid choice.");
+                scanner.nextLine(); // Consume the invalid input
+            }
+        }
+    }
+
+    private static void displayStatisticsMenu() {
+        System.out.println("\nStatistics Menu:");
+        System.out.println("1] No of accounts which have balance more than 1 Lac");
+        System.out.println("2] Show no of account by account type");
+        System.out.println("3] Show no of accounts by account type with sorting");
+        System.out.println("4] Show avg balance by account type");
+        System.out.println("5] List account ids whose account name contains given name");
+        System.out.println("6] Back to Main Menu");
+        System.out.print("Enter your choice: ");
+    }
+
+    private static void printAccountsAbove100000() {
+        List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+        long accountsWithHighBalance = accounts.stream().filter(account -> account.getBalance() > 100000).count();
+        System.out.println("No. of accounts with balance more than 1 Lac: " + accountsWithHighBalance);
+    }
+
+    private static void printAccountTypeCounts() {
+        List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+        Map<String, Long> accountTypeCounts = new HashMap<>();
+
+        for (Account account : accounts) {
+            String accountType = account.getAccountType();
+            accountTypeCounts.put(accountType, accountTypeCounts.getOrDefault(accountType, 0L) + 1);
+        }
+
+        System.out.println("No of accounts by account type:");
+        for (Map.Entry<String, Long> entry : accountTypeCounts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    private static void printSortedAccountTypeCounts() {
+        List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+        Map<String, Long> accountTypeCounts = new HashMap<>();
+
+        for (Account account : accounts) {
+            String accountType = account.getAccountType();
+            accountTypeCounts.put(accountType, accountTypeCounts.getOrDefault(accountType, 0L) + 1);
+        }
+
+        // Create a list of Map entries and sort it by count in descending order
+        List<Map.Entry<String, Long>> sortedAccountTypeCounts = new ArrayList<>(accountTypeCounts.entrySet());
+        sortedAccountTypeCounts.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+        System.out.println("No of accounts by account type (sorted in descending order):");
+        for (Map.Entry<String, Long> entry : sortedAccountTypeCounts) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    private static void printAvgBalanceByAccountType() {
+        List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+        Map<String, Double> avgBalanceByType = new HashMap<>();
+        Map<String, Integer> countByType = new HashMap<>();
+
+        for (Account account : accounts) {
+            String accountType = account.getAccountType();
+            double balance = account.getBalance();
+
+            avgBalanceByType.put(accountType, avgBalanceByType.getOrDefault(accountType, 0.0) + balance);
+            countByType.put(accountType, countByType.getOrDefault(accountType, 0) + 1);
+        }
+
+        System.out.println("Average balance by account type:");
+        for (Map.Entry<String, Double> entry : avgBalanceByType.entrySet()) {
+            String accountType = entry.getKey();
+            double totalBalance = entry.getValue();
+            int count = countByType.get(accountType);
+
+            double avgBalance = totalBalance / count;
+            System.out.println(accountType + ": " + avgBalance);
+        }
+    }
+
+    private static void listAccountIdsByName() {
+        try {
+            System.out.print("Enter a name to search for: ");
+            String searchName = scanner.nextLine().toLowerCase();
+            List<Account> accounts = new ArrayList<>(accountService.getAllAccounts());
+
+            List<String> accountInfoWithName = new ArrayList<>(); // Change to store both ID and type
+
+            for (Account account : accounts) {
+                if (account.getName().toLowerCase().contains(searchName)) {
+                    String accountInfo = "Account ID: " + account.getId() + ", Account Type: " + account.getAccountType();
+                    accountInfoWithName.add(accountInfo);
+                }
+            }
+
+            if (!accountInfoWithName.isEmpty()) {
+                System.out.println("Account IDs and Types with names containing '" + searchName + "':");
+                for (String accountInfo : accountInfoWithName) {
+                    System.out.println(accountInfo);
+                }
+            } else {
+                System.out.println("No accounts found with names containing '" + searchName + "'.");
+            }
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+
+    private static void importAccounts() {
+        try {
+            System.out.print("Enter the path of the file to import from: ");
+            String filePath = scanner.nextLine();
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            int importedCount = 0;
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 5) {
+                    int id = Integer.parseInt(parts[0].trim());
+                    String name = parts[1].trim();
+                    double balance = Double.parseDouble(parts[2].trim());
+                    double roi = Double.parseDouble(parts[3].trim());
+                    String type = parts[4].trim();
+
+                    Account account = new Account(id, name, balance,roi, type);
+
+                    if (accountService.createAccount(account)) {
+                        importedCount++;
+                    }
+                }
+            }
+
+            reader.close();
+            System.out.println(importedCount + " accounts imported successfully.");
+        } catch (IOException e) {
+            System.err.println("Error reading from the file: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Error parsing data from the file: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    private static void exportAccounts() {
+        try {
+            System.out.print("Enter the path of the file to export to: ");
+            String filePath = scanner.nextLine();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+
+            Collection<Account> accounts = accountService.getAllAccounts();
+            for (Account account : accounts) {
+                String line = account.getId() + "," + account.getName() + "," +
+                        account.getBalance() + "," + account.getRoi() + "," +
+                        account.getAccountType();
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+            System.out.println("Accounts exported successfully.");
+        } catch (IOException e) {
+            System.err.println("Error writing to the file: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
