3194719089d6f662c2c366cfd5f727303cb983dd
surajn198 <nandasuraj198@gmail.com>
2023-09-29 09:39:36 +0530

    Assignment-12-junit-Account

---
+++ assignments/corejava/bankingapp-with-junit/bankingapp-with-junit/src/com/indium/bankingapp/model/Account.java
@@ -0,0 +1,60 @@
+package com.indium.bankingapp.model;
+
+public class Account implements Comparable<Account> {
+    private int id;
+    private String name;
+    private double balance;
+    private double roi; // Rate of Interest
+    private String type;
+
+    public Account(int id, String name, double balance, double roi, String type) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.roi = roi;
+        this.type = type;
+    }
+
+    public String getAccountType() {
+        return type;
+    }
+
+    public void setAccountType(String type) {
+        this.type = type;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    public double getRoi() {
+        return roi;
+    }
+
+    public void setRoi(double roi) {
+        this.roi = roi;
+    }
+    @Override
+    public int compareTo(Account other) {
+        return Integer.compare(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Account ID: " + id +
+               "\nAccount Name: " + name +
+               "\nBalance: " + balance +
+               "\nRate of Interest: " + roi +
+               "\nAccount Type: " + type;
+    }
+}
